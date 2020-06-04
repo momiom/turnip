54f4c40b339b4bbffbe87b69bec69b09756096bd
K-Kazuki <kazuki.k.bl8@gmail.com>
2020-06-05 08:41:43 +0900

    OGP headerを設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,9 @@
+const title = 'カブ価予報 | あつまれど うぶつの森'
+const description =
+  '今週のカブ価予報をおとどけします。火曜日の午後ごろまで入力するとそれなりなカブ価予報となるでしょう。'
+const url = 'https://turnip-forecast.netlify.app/'
+const ogImage = `${url}/ogp.png`
+
 export default {
   mode: 'universal',
   /*
@@ -7,7 +13,7 @@ export default {
     htmlAttrs: {
       lang: 'ja'
     },
-    title: 'カブ価予報 | あつまれど うぶつの森',
+    title,
     meta: [
       { charset: 'utf-8' },
       { 'http-equiv': 'content-language', content: 'ja' },
@@ -15,8 +21,35 @@ export default {
       {
         hid: 'description',
         name: 'description',
-        content:
-          '今週のカブ価予報をおとどけします。火曜日の午後ごろまで入力するとそれなりなカブ価予報となるでしょう。'
+        content: description
+      },
+      { hid: 'ogTitle', property: 'og:title', content: title },
+      { hid: 'ogType', property: 'og:type', content: 'website' },
+      { hid: 'ogUrl', property: 'og:url', content: url },
+      {
+        hid: 'ogImage',
+        property: 'og:image',
+        content: ogImage
+      },
+      { property: 'og:site_name', content: title },
+      {
+        hid: 'ogDescription',
+        property: 'og:description',
+        content: description
+      },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { hid: 'twitterSite', name: 'twitter:site', content: title },
+      { hid: 'twitterUrl', name: 'twitter:url', content: url },
+      { hid: 'twitterTitle', name: 'twitter:title', content: title },
+      {
+        hid: 'twitterDescription',
+        name: 'twitter:description',
+        content: description
+      },
+      {
+        hid: 'twitterImage',
+        name: 'twitter:image:src',
+        content: ogImage
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
@@ -79,13 +112,11 @@ export default {
       appleStatusBarStyle: 'black-translucent'
     },
     manifest: {
-      name: 'カブ価予報 | あつまれど うぶつの森',
-      title: 'カブ価予報 | あつまれど うぶつの森',
-      'og:title': 'カブ価予報 | あつまれど うぶつの森',
-      description:
-        '今週のカブ価予報をおとどけします。火曜日の午後ごろまで入力するとそれなりなカブ価予報となるでしょう。',
-      'og:description':
-        '今週のカブ価予報をおとどけします。火曜日の午後ごろまで入力するとそれなりなカブ価予報となるでしょう。',
+      name: title,
+      title,
+      'og:title': title,
+      description,
+      'og:description': description,
       lang: 'ja',
       theme_color: '#FFF9E2',
       background_color: '#FFF9E2',
